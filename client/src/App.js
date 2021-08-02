cf59f2af2a8368956bd4ed4728a151aac33c4b80
jtaylor <jarrede20@gmail.com>
2021-08-02 17:35:04 -0400

    completed the styling and began working on receiving the todos from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,25 @@
+import {useState, useEffect} from 'react';
+
+const API_BASE = "http://localhost:3001";
+
 function App() {
+    const [todos, setTodos] = useState([]);
+    const [popupActive, setPopupActive] = useState(false); 
+    const [newTodo, setNewTodo] = useState("");
+
+    useEffect(() => { 
+        GetTodos();
+    }, [])
+
+    const GetTodos = () => {
+        fetch(API_BASE + "/todos")
+            .then(res => res.json())
+            .then(data => setTodos(data))
+            .catch(err => console.error("ERROR: ", err));
+    }
+
+
+
     return (
         <div className="App">
             <h1>Welcome, Jarred</h1>
